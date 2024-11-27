4bc3084a908d377fa3ebc6635cc54dd93f3218a6
heosangbeom <luv_3_u@naver.com>
2024-11-27 17:04:50 +0900

    ✨feat(setting): 세팅
    - 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,13 @@ repositories {
 }
 
 dependencies {
+    //    kafka
+    implementation ("org.apache.kafka:kafka-clients:3.7.1")
+//    slf4j
+    implementation ("org.slf4j:slf4j-api:1.7.36")
+    implementation ("org.slf4j:slf4j-simple:1.7.36")
+//    faker
+    implementation ("com.github.javafaker:javafaker:1.0.2")
     testImplementation(kotlin("test"))
 }
 
